fix mean and variance computation, fix logging validation, remove pooling increase stride

loader.py
@@ -8,6 +8,9 @@ import os
 import random
 
 class Loader:
+
+    max_len = 500
+
     def __init__(self, data_path, batch_size):
         self.batch_size = batch_size
         self.data_path = data_path
@@ -36,7 +39,12 @@ class Loader:
         self._label_to_int = {l : i for i, l in self._int_to_label.iteritems()}
 
         self._train = self.preprocess(self._train)
+        self._train = filter(lambda x : x[0].shape[0] < Loader.max_len,
+                             self._train)
         self._valid = self.preprocess(self._valid)
+        self.compute_mean_std()
+        self.normalize(self._train)
+        self.normalize(self._valid)
         # TODO, awni, some classes in test but not in train
         #self._test = self.preprocess(self._test)
 
@@ -49,14 +57,17 @@ class Loader:
             batch_labels = labels[i:i + batch_size]
             yield (batch_data, batch_labels)
 
-    def mean_and_std(self):
+    def normalize(self, dataset):
+        for example in dataset:
+            example[0][:] = (example[0] - self.mean) / self.std
+
+    def compute_mean_std(self):
         """
         Estimates the mean and std over the training set.
         """
         all_dat = np.hstack(w for w, _ in self._train)
-        mean = np.mean(all_dat)
-        std = np.std(all_dat)
-        return mean, std
+        self.mean = np.mean(all_dat)
+        self.std = np.std(all_dat)
 
     @property
     def vocab_size(self):
@@ -111,4 +122,3 @@ if __name__ == "__main__":
         assert len(labels) == batch_size, "Bad labels size."
     assert (e + 1) == int(len(ldr.train) / batch_size), \
             "Bad number of batches."
-    mean, std = ldr.mean_and_std()

models/rnn.py
@@ -6,25 +6,21 @@ import tensorflow.contrib.layers as tfl
 
 class RNN:
 
-    def init_inference(self, config, mean=0, std=1):
+    def init_inference(self, config):
         self.output_dim = num_labels = config['output_dim']
         self.batch_size = batch_size = config['batch_size']
 
-        mean = tf.Variable(mean, dtype=tf.float32, trainable=False)
-        std = tf.Variable(std, dtype=tf.float32, trainable=False)
-
         self.inputs = inputs = tf.placeholder(tf.float32, shape=(batch_size, None))
-        inputs = (inputs - mean) / std
         acts = tf.reshape(inputs, (batch_size, 1, -1, 1)) #NHWC
 
-        acts = tfl.convolution2d(acts, num_outputs=32, kernel_size=[1, 8])
-        acts = tfl.max_pool2d(acts, kernel_size=[1, 2], padding='SAME')
+        acts = tfl.convolution2d(acts, num_outputs=32, kernel_size=[1, 8], stride=4)
+        #acts = tfl.max_pool2d(acts, kernel_size=[1, 2], padding='SAME')
 
-        acts = tfl.convolution2d(acts, num_outputs=64, kernel_size=[1, 8])
-        acts = tfl.max_pool2d(acts, kernel_size=[1, 2], padding='SAME')
+        acts = tfl.convolution2d(acts, num_outputs=64, kernel_size=[1, 8], stride=4)
+        #acts = tfl.max_pool2d(acts, kernel_size=[1, 2], padding='SAME')
 
-        acts = tfl.convolution2d(acts, num_outputs=128, kernel_size=[1, 8])
-        acts = tfl.max_pool2d(acts, kernel_size=[1, 2], padding='SAME')
+        acts = tfl.convolution2d(acts, num_outputs=128, kernel_size=[1, 8], stride=4)
+        #acts = tfl.max_pool2d(acts, kernel_size=[1, 2], padding='SAME')
 
         acts = tf.squeeze(tf.reduce_mean(acts, reduction_indices=2))
         acts = tfl.fully_connected(acts, 256)
@@ -39,8 +35,10 @@ class RNN:
 
     def init_train(self, config):
         learning_rate = config['learning_rate']
-        momentum = config['momentum']
-        ema = tf.train.ExponentialMovingAverage(0.99)
+        self.momentum = tf.Variable(config['momentum'],
+                                    trainable=False,
+                                    dtype=tf.float32)
+        ema = tf.train.ExponentialMovingAverage(0.95)
         ema_op = ema.apply([self.loss, self.acc])
         self.avg_loss = ema.average(self.loss)
         self.avg_acc = ema.average(self.acc)
@@ -49,10 +47,13 @@ class RNN:
         tf.scalar_summary("Accuracy", self.acc)
 
         self.it = tf.Variable(0, trainable=False, dtype=tf.int64)
-        optimizer = tf.train.MomentumOptimizer(learning_rate, momentum)
+        optimizer = tf.train.MomentumOptimizer(learning_rate, self.momentum)
         train_op = optimizer.minimize(self.loss, global_step=self.it)
         with tf.control_dependencies([train_op]):
             self.train_op = tf.group(ema_op)
+
+    def set_momentum(self):
+        self.momentum.assign(0.999)
 
     def feed_dict(self, inputs, labels=None):
         """

train.py
@@ -19,17 +19,20 @@ FLAGS = tf.flags.FLAGS
 
 def run_epoch(model, data_loader, session, summarizer):
     summary_op = tf.merge_all_summaries()
+    model.set_momentum()
 
     for batch in data_loader.batches(data_loader.train):
-        ops = [model.train_op, model.avg_loss, model.avg_acc, model.it, summary_op]
+        ops = [model.train_op, model.avg_loss,
+               model.avg_acc, model.it, summary_op]
         res = session.run(ops, feed_dict=model.feed_dict(*batch))
         _, loss, acc, it, summary = res
-        summarizer.add_summary(summary, it)
+        summarizer.add_summary(summary, global_step=it)
         if it % 100 == 0:
             msg = "Iter {}: AvgLoss {:.3f}, AvgAcc {:.3f}"
             print(msg.format(it, loss, acc))
 
 def run_validation(model, data_loader, session, summarizer):
+    it = model.it.eval(session)
     results = []
     for batch in data_loader.batches(data_loader.valid):
         ops = [model.acc, model.loss]
@@ -37,9 +40,9 @@ def run_validation(model, data_loader, session, summarizer):
         results.append(res)
     acc, loss = np.mean(zip(*results), axis=1)
     summary = utils.make_summary("Dev Accuracy", float(acc))
-    summarizer.add_summary(summary)
+    summarizer.add_summary(summary, global_step=it)
     summary = utils.make_summary("Dev Loss", float(loss))
-    summarizer.add_summary(summary)
+    summarizer.add_summary(summary, global_step=it)
     msg = "Validation: Loss {:.3f}, Acc {:.3f}"
     print(msg.format(loss, acc))
 
@@ -64,8 +67,7 @@ def main(argv=None):
 
     with tf.Graph().as_default(), tf.Session() as sess:
         tf.set_random_seed(config['seed'])
-        mean, std = data_loader.mean_and_std()
-        model.init_inference(config['model'], mean=mean, std=std)
+        model.init_inference(config['model'])
         model.init_loss()
         model.init_train(config['optimizer'])
         tf.initialize_all_variables().run()