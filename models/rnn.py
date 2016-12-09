5d7c9b1ad5419570bc0d16b61cddd8deea4f2d85
awni <awni.hannun@gmail.com>
2016-12-09 14:48:58 -0800

    change cell type in config

--- models/rnn.py
+++ models/rnn.py
@@ -29,33 +29,43 @@ class RNN(model.Model):
 
         bidirectional = config['rnn'].get('bidirectional', False)
         rnn_dim = config['rnn']['dim']
+        cell_type = config['rnn'].get('cell_type', 'gru')
         if bidirectional:
-            acts = _bi_rnn(acts, rnn_dim)
+            acts = _bi_rnn(acts, rnn_dim, cell_type)
         else:
-            acts = _rnn(acts, rnn_dim)
+            acts = _rnn(acts, rnn_dim, cell_type)
 
         acts = tf.reduce_mean(acts, reduction_indices=1)
         self.logits = tfl.fully_connected(acts, self.output_dim)
 
-def _rnn(acts, input_dim, scope=None):
-    cell = tf.nn.rnn_cell.GRUCell(input_dim)
+def _rnn(acts, input_dim, cell_type, scope=None):
+    if cell_type == 'gru':
+        cell = tf.nn.rnn_cell.GRUCell(input_dim)
+    elif cell_type == 'lstm':
+        cell = tf.nn.rnn_cell.LSTMCell(input_dim)
+    else:
+        msg = "Invalid cell type {}".format(cell_type)
+        raise ValueError(msg)
+
     acts, _ = tf.nn.dynamic_rnn(cell, acts,
                   dtype=tf.float32, scope=scope)
     return acts
 
-def _bi_rnn(acts, input_dim):
+def _bi_rnn(acts, input_dim, cell_type):
     """
     For some reason tf.bidirectional_dynamic_rnn requires a sequence length.
     """
     # Forwards
     with tf.variable_scope("fw") as fw_scope:
-        acts_fw = _rnn(acts, input_dim, scope=fw_scope)
+        acts_fw = _rnn(acts, input_dim, cell_type,
+                       scope=fw_scope)
 
     # Backwards
     with tf.variable_scope("bw") as bw_scope:
         reverse_dims = [False, True, False]
         acts_bw = tf.reverse(acts, dims=reverse_dims)
-        acts_bw = _rnn(acts_bw, input_dim, scope=bw_scope)
+        acts_bw = _rnn(acts_bw, input_dim, cell_type,
+                       scope=bw_scope)
         acts_bw = tf.reverse(acts, dims=reverse_dims)
 
     # Sum the forward and backward states.
