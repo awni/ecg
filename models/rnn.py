977c520aadafbf1cf0098575cb50d1ffc07a100c
awni <awni.hannun@gmail.com>
2016-12-16 17:07:59 -0800

    include probs in rnn

--- models/rnn.py
+++ models/rnn.py
@@ -10,8 +10,6 @@ class RNN(model.Model):
         self.output_dim = num_labels = config['output_dim']
         self.batch_size = batch_size = config['batch_size']
 
-
-
         self.inputs = inputs = tf.placeholder(tf.float32, shape=(batch_size, None))
         acts = tf.reshape(inputs, (batch_size, -1, 1, 1))
 
@@ -36,6 +34,7 @@ class RNN(model.Model):
             acts = _rnn(acts, rnn_dim, cell_type)
 
         self.logits = tfl.fully_connected(acts, self.output_dim)
+        self.probs = tf.nn.softmax(self.logits)
 
 def _rnn(acts, input_dim, cell_type, scope=None):
     if cell_type == 'gru':
