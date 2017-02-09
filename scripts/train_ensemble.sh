c5a6a24280376cb446b9376fbd75e79bb72c4d6b
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-02-08 19:53:24 -0800

    Add script to train ensemble model

--- scripts/train_ensemble.sh
+++ scripts/train_ensemble.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-cd ..
-python ecg/train.py data configs/default.json $1
+for DEVICE in 0 1 2 3
+do
+    DEVICE_UTILIZATION=$(nvidia-smi --id=$DEVICE --query-gpu=utilization.gpu --format=csv,nounits,noheader)
+    if [ $(($DEVICE_UTILIZATION + 0)) -gt 2 ]
+        then
+            echo "GPU $DEVICE not free"
+        else
+            echo "GPU $DEVICE free"
+            export CUDA_VISIBLE_DEVICES=$DEVICE
+            python ecg/train.py data configs/default.json $1 > $DEVICE.out 2>&1 &
+    fi
+echo $OUTPUT
+done
