cfd84edd7ef6b9f756ea8720c98fe04567193ea8
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-02-08 23:08:00 -0800

    Remove screen usage

--- scripts/train_ensemble.sh
+++ scripts/train_ensemble.sh
@@ -8,7 +8,8 @@ do
         else
             echo "GPU $DEVICE free"
             export CUDA_VISIBLE_DEVICES=$DEVICE
-            screen -d -m bash -c "python ecg/train.py data configs/default.json $1 > $DEVICE.out 2>&1"
+            python ecg/train.py data configs/default.json $1 -v 2 > $DEVICE.out 2>&1 &
             echo "Started training script on $DEVICE..."
+            
     fi
 done
