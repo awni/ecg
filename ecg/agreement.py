7cb145432a785bceb960fa8c9ca33fc4180d934d
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-02-15 21:22:48 -0800

    Fix agreement script

--- ecg/agreement.py
+++ ecg/agreement.py
@@ -1,5 +1,4 @@
 import evaluate
-import util
 import json
 import load
 import itertools
@@ -9,11 +8,11 @@ NUM_LABELLERS = 3
 classes = None
 
 
-def get_labels_of_labeler(args, params, index):
+def get_labels_of_labeler(params, index):
     global classes
     assert(index >= 0 and index < NUM_LABELLERS)
     params["epi_ext"] = "_rev" + str(index) + ".episodes.json"
-    dl = load.load_train(args, params)
+    dl = load.load_train(params)
     if classes is None:
         classes = dl.classes
     else:
@@ -22,9 +21,9 @@ def get_labels_of_labeler(args, params, index):
 
 
 def agreement():
-    args = util.get_object_from_dict(data_path="./data/label_review")
     params = json.load(open('./configs/test.json', 'r'))
-    all_labels = [get_labels_of_labeler(args, params, i) for
+    params["data_path"] = "./data/label_review"
+    all_labels = [get_labels_of_labeler(params, i) for
                   i in range(NUM_LABELLERS)]
     for pair in itertools.combinations(range(NUM_LABELLERS), 2):
         print("Agreement between " + str(pair))
