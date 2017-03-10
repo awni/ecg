ae72ae0ce97ca6b62a690437b609fb298d7253c1
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-03-10 14:08:14 -0800

    New human agreement method

--- ecg/agreement.py
+++ ecg/agreement.py
@@ -3,38 +3,30 @@ import json
 import load
 import itertools
 import numpy as np
-
-NUM_LABELLERS = 3
-classes = None
+import argparse
 
 
-def get_labels_of_labeler(params, index):
-    global classes
-    assert(index >= 0 and index < NUM_LABELLERS)
-    params["epi_ext"] = "_rev" + str(index) + ".episodes.json"
-    dl = load.load_train(params)
-    if classes is None:
-        classes = dl.classes
-    else:
-        assert(classes == dl.classes)
-    return np.argmax(dl.y_test, axis=-1)
+def agreement(args, params):
+    x, ground_truths, classes = load.load_test(params)
+    
+    num_reviewers = params.get("num_reviewers", 3)
 
+    select_index = np.random.randint(0, num_reviewers, size=len(ground_truths))
+    human_prediction_mask = np.zeros((select_index.size, select_index.max()+1), dtype='bool')
+    human_prediction_mask[np.arange(select_index.size), select_index]  = 1
+    
+    predictions = ground_truths[human_prediction_mask]
+    new_ground_truths = ground_truths[~human_prediction_mask].reshape((len(ground_truths), num_reviewers - 1, -1))
 
-def agreement():
-    params = json.load(open('./configs/test.json', 'r'))
-    params["data_path"] = "./data/label_review"
-    all_labels = [get_labels_of_labeler(params, i) for
-                  i in range(NUM_LABELLERS)]
-    for pair in itertools.combinations(range(NUM_LABELLERS), 2):
-        print("Agreement between " + str(pair))
-        evaluate.compute_scores(
-            all_labels[pair[0]],
-            all_labels[pair[1]],
-            classes,
-            confusion_table=False,
-            plot=False)
+    predictions = np.tile(predictions, (num_reviewers - 1, 1))
+    new_ground_truths = np.swapaxes(new_ground_truths, 0, 1)
+
+    evaluate.compute_scores(new_ground_truths, predictions, classes)
 
 
 if __name__ == '__main__':
-    agreement()
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", help="path to config file")
+    args = parser.parse_args()
+    params = json.load(open(args.config_file, 'r'))
+    agreement(args, params)
