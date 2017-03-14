af499a1277068cc4cc2317e9e42961596fb90485
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-03-13 19:46:53 -0700

    Surround plot with try catch block

--- ecg/plot.py
+++ ecg/plot.py
@@ -3,19 +3,22 @@ import util
 
 
 def plot_confusion_matrix(cm, classes):
-    import matplotlib
-    matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-    cmap = plt.cm.Blues
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title('Confusion matrix')
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=90)
-    plt.yticks(tick_marks, classes)
+    try:
+        import matplotlib
+        matplotlib.use('Agg')
+        import matplotlib.pyplot as plt
+        cmap = plt.cm.Blues
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title('Confusion matrix')
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=90)
+        plt.yticks(tick_marks, classes)
 
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.show()
-    plt.savefig(util.get_plot_path('confusion'))
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.show()
+        plt.savefig(util.get_plot_path('confusion'))
+    except:
+        print("Skipping plot")
