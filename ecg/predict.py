d9f3ea962edeb6113a64b2030f8051ae7d8a083e
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-04-23 16:14:17 -0700

    Cache individual preds

--- ecg/predict.py
+++ ecg/predict.py
@@ -6,15 +6,15 @@ memory = Memory(cachedir='./cache')
 
 
 @memory.cache
+def get_model_pred_probs(model_path, x):
+    from keras.models import load_model
+    model = load_model(model_path)
+    probs = model.predict(x, verbose=1)
+    return probs
+
+
 def get_ensemble_pred_probs(model_paths, x, geo_mean=False):
     print("Averaging " + str(len(model_paths)) + " model predictions...")
-
-    def get_model_pred_probs(model_path, x):
-        from keras.models import load_model
-        model = load_model(model_path)
-        probs = model.predict(x, verbose=1)
-        return probs
-
     all_model_probs = [get_model_pred_probs(model_path, x)
                        for model_path in model_paths]
     if geo_mean is True:
