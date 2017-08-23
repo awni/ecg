178c20b5a9dcbc33e4a5c5e379c93c499818736d
Rajpurkar <pranav.rajpurkar@gmail.com>
2017-08-23 16:48:25 -0700

    Add initial predict fn

--- ecg/predict.py
+++ ecg/predict.py
@@ -1,9 +1,12 @@
 from builtins import str
+import argparse
 import numpy as np
+import json
+import load
+import util
 from joblib import Memory
-import scipy.stats.mstats
-memory = Memory(cachedir='./cache')
 
+memory = Memory(cachedir='./cache')
 
 @memory.cache
 def get_model_pred_probs(model_path, x):
@@ -13,12 +16,43 @@ def get_model_pred_probs(model_path, x):
     return probs
 
 
-def get_ensemble_pred_probs(model_paths, x, geo_mean=False):
+def get_ensemble_pred_probs(model_paths, x):
     print("Averaging " + str(len(model_paths)) + " model predictions...")
     all_model_probs = [get_model_pred_probs(model_path, x)
                        for model_path in model_paths]
-    if geo_mean is True:
-        probs = scipy.stats.mstats.gmean(all_model_probs, axis=0)
-    else:
-        probs = np.mean(all_model_probs, axis=0)
+    probs = np.mean(all_model_probs, axis=0)
     return probs
+
+def get_saved_pred_probs(path):
+    return np.load(path)
+
+
+def predict(args, train_params, test_params):
+    x, gt, processor, loader = load.load_test(
+        test_params,
+        train_params=train_params,
+        split=args.split)
+    probs = get_ensemble_pred_probs(args.model_paths, x)
+    save_prediction_probs(probs)
+
+def save_prediction_probs(probs):
+    np.save('./preds', probs)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("test_config_file", help="path to config file")
+    parser.add_argument(
+        'model_paths',
+        nargs='+',
+        help="path to models")
+    parser.add_argument("--split", help="train/val", choices=['train', 'test'],
+                        default='test')
+    args = parser.parse_args()
+    train_params = util.get_model_params(args.model_paths[0])  # FIXME: bug
+    test_params = train_params.copy()
+    test_new_params = json.load(open(args.test_config_file, 'r'))
+    test_params.update(test_new_params)
+    if "label_review" in test_new_params["data_path"]:
+        assert(args.split == 'test')
+    predict(args, train_params, test_params)
