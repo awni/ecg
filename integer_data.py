0ad8edc179003e169e2402bcd5f94bf82b9aa3c7
Namshik Kim <physhik@gmail.com>
2019-03-19 21:39:25 -0700

    Update integer_data.py

--- integer_data.py
+++ integer_data.py
@@ -1,27 +1,44 @@
+from tqdm import tqdm 
+
 def digitize():
   import os
-  path = 'clean_30sec_recs'
+  path = 'clean_30sec_recs/'
   files = os.listdir(path)
-  l = []
   
-  for name in files:
+  d = dict()
+  for name in tqdm(files):
+    l = []
+    id = None
     if ".ecg" in name:
-      d = dict()
+      id = name[:-4]
       def makeIntlist():
         import struct
-        with open( path +"/"+ name ,"rb") as n:
+        with open( path + name ,"rb") as n:
           while True:
             try :
               l.append(struct.unpack('i', n.read(4))[0])
             except:
               break
+    if id != None:
+      for i in range(3):
+        import json
+        arg = path + id + "_grp"+str(i)+".episodes.json"
+        if os.path.exists(arg):
+          id2 = arg
+      with open(id2, "r") as read_file:
+          ann=json.load(read_file)
+          ann = ann["episodes"][0]["rhythm_name"]
         
       makeIntlist()
-      import pandas as pd
-      d["data"] = l
-      df = pd.DataFrame.from_dict(d)
-      savepath = "data/"+name[-15:-10]+".csv"
-      df.to_csv(savepath, index = False)   
-       
+    
+    import pandas as pd
+    if id:
+      print(ann+id)
+      d["200 " +ann + " " + id] = l
+  df = pd.DataFrame.from_dict(d)
+  return df 
+    
 if __name__ == '__main__':
-    digitize()
+    df = digitize()
+    savepath = "data.csv"
+    df.to_csv(savepath, index = False) 
