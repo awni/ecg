1beefd0d505ae1f4b0440443062e1566e6f2efdc
felix.grzelka <felix.grzelka@student.hpi.de>
2019-11-22 23:14:29 +0000

    fixing depricated pickle import and file modes

--- ecg/util.py
+++ ecg/util.py
@@ -1,13 +1,13 @@
 import os
-import cPickle as pickle
+import pickle as pickle
 
 def load(dirname):
     preproc_f = os.path.join(dirname, "preproc.bin")
-    with open(preproc_f, 'r') as fid:
+    with open(preproc_f, 'rb') as fid:
         preproc = pickle.load(fid)
     return preproc
 
 def save(preproc, dirname):
     preproc_f = os.path.join(dirname, "preproc.bin")
-    with open(preproc_f, 'w') as fid:
+    with open(preproc_f, 'wb') as fid:
         pickle.dump(preproc, fid)
