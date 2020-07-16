a7ca222b5321f56642f5e2f31fb814657663c3fc
CATERINA-SEUL <dalgona91@gmail.com>
2020-07-16 09:45:57 +0900

    Fix : blacklist error

--- examples/irhythm/build_datasets.py
+++ examples/irhythm/build_datasets.py
@@ -21,8 +21,10 @@ def get_all_records(path, blacklist=set()):
             if patient_id(filename) not in blacklist:
                 records.append(os.path.abspath(
                     os.path.join(root, filename)))
+                
     return records
 
+## 파일에서 앞에 부분을 추출하여 patient id로 사용
 def patient_id(record):
     return os.path.basename(record).split("_")[0]
 
@@ -66,6 +68,7 @@ def make_labels(episodes):
     labels = labels[:trunc_samp]
     return labels
 
+## 왜 patient id를 추출한게 black list이지?
 def build_blacklist(blacklist_paths):
     print('Building blacklist...')
     blacklist = set()
@@ -75,7 +78,7 @@ def build_blacklist(blacklist_paths):
             blacklist.add(patient_id(record))
     return blacklist
 
-def construct_dataset(records, epi_ext='.episodes.json'):
+def construct_dataset(records, epi_ext='*.episodes.json'):
     data = []
     for record in tqdm.tqdm(records):
         labels = make_labels(load_episodes(record, epi_ext))
@@ -92,7 +95,7 @@ def stratify(records, dev_frac):
     dev = [r for r in records if patient_id(r) in dev_pids] 
     return train, dev 
 
-def load_train(data_path, dev_frac, blacklist_paths):
+"""def load_train(data_path, dev_frac, blacklist_paths):
     blacklist = build_blacklist(blacklist_paths)
     records = get_all_records(data_path, blacklist)
     train, dev = stratify(records, dev_frac)
@@ -100,6 +103,20 @@ def load_train(data_path, dev_frac, blacklist_paths):
     train = construct_dataset(train)
     print("Constructing dev...")
     dev = construct_dataset(dev)
+    return train, dev"""
+
+def load_train(data_path, dev_frac, epi_ext):
+    records = get_all_records(data_path)
+    train, dev = stratify(records, dev_frac)
+    print("Constructing train...")
+    train = construct_dataset(train, epi_ext)
+    print("Constructing dev...")
+    dev = construct_dataset(dev, epi_ext)
+
+    reviewers = [load_rev_id(r, epi_ext) for r in records]
+    train = [(e,l,r) for (e, l), r in zip(train, reviewers)]
+    dev = [(e,l,r) for (e,l),r in zip(dev, reviewers)]
+
     return train, dev
 
 def load_rev_id(record, epi_ext):
@@ -117,12 +134,13 @@ def load_test(data_path, epi_ext):
     test = construct_dataset(records, epi_ext)
     # Get the reviewer id
     reviewers = [load_rev_id(r, epi_ext) for r in records]
+    print(reviewers)
     test = [(e, l, r)
             for (e, l), r in zip(test, reviewers)]
     return test
 
 def make_json(save_path, dataset):
-    with open(save_path, 'w') as fid:
+    with open(save_path, 'w') as fid:ㅊㅇ ../
         for d in dataset:
             datum = {'ecg' : d[0],
                      'labels' : d[1]}
@@ -133,17 +151,18 @@ def make_json(save_path, dataset):
 
 
 if __name__ == "__main__":
-    data_dir = "/deep/group/med/irhythm/ecg/clean_30sec_recs/"
-    blacklist_paths = [
-            os.path.join(data_dir, "label_review/CARDIOL_MAY_2017/"),
-            os.path.join(data_dir, "batches/kids_blacklist"),
-            os.path.join(data_dir, "batches/vf_blacklist")]
-    data_path = os.path.join(data_dir, "batches")
-    dev_frac = 0.1
-    train, dev = load_train(data_path, dev_frac, blacklist_paths)
+    data_dir = "/Users/caterina/Documents/dev/ecg/examples/irhythm/ecg_data/CARDIOL_MAY_2017"
+    """blacklist_paths = [
+            os.path.join(data_dir, "ecg_data"),]"""
+    blacklist_paths = ""
+    # data_path = os.path.join(data_dir, "CARDIOL_MAY_2017")
+    data_path = os.path.abspath(data_dir)
+    dev_frac = 0.2
+    train, dev = load_train(data_path, dev_frac, '_grp*.episodes.json')
     make_json("train.json", train)
     make_json("dev.json", dev)
-    test_dir = os.path.join(data_dir, "label_review/CARDIOL_UNIQ_P/")
+    # test_dir = os.path.join(data_dir, "label_review/CARDIOL_UNIQ_P/")
+    test_dir = os.path.abspath(data_dir)
     test = load_test(test_dir, '_grp*.episodes.json')
     make_json("test.json", test)
     for i in range(6):
